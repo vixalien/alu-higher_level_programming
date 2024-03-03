fc03069ef56dd4b088645e0b285ce27db72e80c8
Angelo Verlain <geoangercola@gmail.com>
2024-03-03 12:20:05 +0200

    Advanced tasks

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return (None)
 
-
-def best_score(a_):
-    if a_ is None:
-        return "None"
-    elif len(a_) > 0:
-        dict_me = dict(sorted(a_.items(), key=lambda x: x[1], reverse=True))
-        list_me = list(dict_me)
-        return list_me[0]
-    else:
-        return "None"
+    return (max(a_dictionary, key=a_dictionary.get))
