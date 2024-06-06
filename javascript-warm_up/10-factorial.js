1521a15112733f9a0e99089a58a5d825e01f2b34
Angelo Verlain <geoangercola@gmail.com>
2024-06-06 19:56:22 +0200

    fix advanced tasks

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,14 +1,12 @@
 #!/usr/bin/node
-function factorial (num) {
-  if (num === 1) {
-    return 1;
-  } else {
-    return num * factorial(num - 1);
+function factorial (n) {
+  if (n < 0) {
+    return (-1);
   }
+  if (n === 0 || isNaN(n)) {
+    return (1);
+  }
+  return (n * factorial(n - 1));
 }
 
-if (isNaN(process.argv[2])) {
-  console.log(1);
-} else {
-  console.log(factorial(parseInt(process.argv[2])));
-}
+console.log(factorial(Number(process.argv[2])));
--- javascript-warm_up/100-let_me_const.js
+++ javascript-warm_up/100-let_me_const.js
@@ -1,5 +1,2 @@
 #!/usr/bin/node
-module.exports = function() {
-  require('./100-main')
-  myVar = 333;
-}
+myVar = 333;
